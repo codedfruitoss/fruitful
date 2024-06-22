ba2348c1d03094665afac84a12a34b77965a35f1
Ashish Chandwani <ashishchandwani21@gmail.com>
2024-06-22 15:44:26 +0530

    Start , Stop , Pause with Buttons

--- app/index.tsx
+++ app/index.tsx
@@ -1,47 +1,89 @@
 import { NormalText } from "@/components/StyledText";
-import React, { useEffect, useState } from "react";
+import React, { useRef, useState } from "react";
 import { View, Pressable } from "react-native";
+import {
+	Gesture,
+	GestureDetector,
+	GestureHandlerRootView,
+} from "react-native-gesture-handler";
 
 function getMinutes(s: number) {
-  return Math.floor(s / 60);
+	return Math.floor(s / 60);
 }
 
 function getDisplayTime(seconds: number) {
-  const minutes = getMinutes(seconds);
-  const s = seconds % 60;
-  return `${minutes}:${s}`;
+	const minutes = getMinutes(seconds);
+	const s = seconds % 60;
+	return `${minutes}:${s}`;
 }
 
+const intialPTime = 1500;
+
 export default function Main() {
-  const [ptime, setPTime] = useState(1500);
-  // const [timerOn, setTimerOn] = useState(false)
-  // let myTimer: any
+	const [ptime, setPTime] = useState(intialPTime);
+	const intervalRef = useRef<any>(null);
 
+	const tap = Gesture.Tap().onEnd(() => {
+		startTimer();
+	});
 
-  useEffect(() => {
-    setInterval(() => { setPTime((a) => a > 0 ? a - 1 : a); }, 1000);
-  }, []);
+	function startTimer() {
+		if (!intervalRef.current) {
+			const id = setInterval(() => {
+				setPTime((a) => (a > 0 ? a - 1 : a));
+			}, 1000);
+			intervalRef.current = id;
+		}
+	}
 
-  // useEffect(() => {
-  //   console.log("inside useeee", timerOn)
-  //   // const myTimer = setInterval(() => { setPTime((a) => a > 0 ? a - 1 : a); }, 1000);
-  //   if (!timerOn) {
-  //     clearInterval(myTimer)
-  //   }
-  //   else {
-  //     clearInterval(myTimer)
-  //     myTimer = setInterval(() => { setPTime((a) => a > 0 ? a - 1 : a); }, 1000);
-  //   }
-  // }, [timerOn])
+	function stopTimer() {
+		clearInterval(intervalRef?.current);
+		intervalRef.current = null;
+		setPTime(intialPTime);
+	}
 
-  return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: "black" }}>
-        {/* <NormalText style={{ margin: "auto" }}>{getDisplayTime(ptime)}</NormalText> */}
-        <Pressable style={{ margin: "auto" }} onPress={() => { }}>
-          <NormalText>{getDisplayTime(ptime)}</NormalText>
-        </Pressable>
-      </View>
-    </View>
-  );
+	function pauseTimer() {
+		clearInterval(intervalRef.current);
+		intervalRef.current = null;
+	}
+
+	return (
+		<GestureHandlerRootView
+			style={{
+				flex: 1,
+				alignItems: "center",
+				justifyContent: "center",
+				backgroundColor: "black",
+			}}
+		>
+			<GestureDetector gesture={tap}>
+				<NormalText
+					style={{
+						fontSize: 86,
+					}}
+				>
+					{getDisplayTime(ptime)}
+				</NormalText>
+			</GestureDetector>
+			<View>
+				<Pressable onPress={() => startTimer()}>
+					<NormalText style={{ fontSize: 24 }}>Start</NormalText>
+				</Pressable>
+			</View>
+			<View>
+				<Pressable onPress={() => stopTimer()}>
+					<NormalText style={{ fontSize: 24, marginTop: 24 }}>
+						Stop
+					</NormalText>
+				</Pressable>
+			</View>
+			<View>
+				<Pressable onPress={() => pauseTimer()}>
+					<NormalText style={{ fontSize: 24, marginTop: 24 }}>
+						Pause
+					</NormalText>
+				</Pressable>
+			</View>
+		</GestureHandlerRootView>
+	);
 }
