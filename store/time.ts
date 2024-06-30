5aec43cca254a6df09a5dbf8b4739a757f89a5d4
Ashish Chandwani <ashishchandwani21@gmail.com>
2024-06-30 14:06:55 +0530

    Playing Sound

--- store/time.ts
+++ store/time.ts
@@ -2,9 +2,15 @@ import { Dayjs } from "dayjs";
 import { atom } from "jotai";
 
 const workTimeAtom = atom(1500);
-const breakTimeAtom = atom(300);
+const breakTimeAtom = atom(90);
 const workTimeLogAtom = atom<{ startTime: Dayjs; endTime: Dayjs }[]>([]);
 const breakTimeLogAtom = atom<{ startTime: Dayjs; endTime: Dayjs }[]>([]);
-const warningTimeAtom = atom(60)
+const warningTimeAtom = atom(60);
 
-export { workTimeAtom, breakTimeAtom, workTimeLogAtom, breakTimeLogAtom, warningTimeAtom };
+export {
+	workTimeAtom,
+	breakTimeAtom,
+	workTimeLogAtom,
+	breakTimeLogAtom,
+	warningTimeAtom,
+};
